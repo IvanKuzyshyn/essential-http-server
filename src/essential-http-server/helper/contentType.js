96aa7f32b4f56f59db565bea07302cd4ef69500a
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2017-12-03 20:28:09 +0200

    master Set part of flow configuration

--- src/essential-http-server/helper/contentType.js
+++ src/essential-http-server/helper/contentType.js
@@ -1,6 +1,8 @@
+// @flow
+
 import types from '../constants/content-types';
 
-export default function setContentType(type) {
+export default function setContentType(type: string): string {
   if(!(type in types)) return 'application/json; charset=utf-8';
 
   return types[type];
--- src/essential-http-server/server/EssentialHttpServer.js
+++ src/essential-http-server/server/EssentialHttpServer.js
@@ -1,14 +1,27 @@
+/* @flow */
+
 import net from 'net';
-import defaultConfig from '../config/server';
+import type defaultConfig from '../config/server';
 import ResponseFormatter from '../service/ResponseFormatter';
+
+type ServerConfigType = {
+  port?: number,
+  rootDir?: string,
+  handleRobots?: boolean,
+};
+
+interface ServerInterface {
+  config: ServerConfigType;
+}
 
 export const STATUS_RUNNED = 'runned';
 export const STATUS_HALT = 'halt';
 
 export default class EssentialHttpServer {
+  config: ServerConfigType;
 
-  constructor(config) {
-    this.config = {...defaultConfig, ...config};
+  constructor(config: ServerConfigType) {
+    this.config = { ...defaultConfig, ...config };
     this.isStarted = false;
 
     this.construct();
@@ -18,10 +31,10 @@ export default class EssentialHttpServer {
       stop: this.stop,
       restart: this.restart,
       getStatus: this.getStatus,
-    }
+    };
   }
 
-  construct() {
+  construct = (): void => {
     this.server = net.createServer();
 
     this.server.on('connection', socket => {
@@ -31,35 +44,32 @@ export default class EssentialHttpServer {
         formatter.send(data, socket);
       });
     });
-  }
+  };
 
-  start = () => {
-    if(this.isStarted) {
+  start = (): void => {
+    if (this.isStarted) {
       global.console.warn('Server has been already started!');
 
       return;
     }
 
-    this.server.listen({port: this.config.port}, () => {
+    this.server.listen({ port: this.config.port }, () => {
       global.console.log(`Server is running. PORT ${this.server.address()}`);
     });
   };
 
-  stop = () => {
+  stop = (): void => {};
 
-  };
-
-  restart = () => {
+  restart = (): void => {
     this.stop();
     this.start();
   };
 
-  getStatus = () => {
-    if(this.isStarted) {
+  getStatus = (): string => {
+    if (this.isStarted) {
       return STATUS_RUNNED;
     }
 
     return STATUS_HALT;
   };
-
 }
