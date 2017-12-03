261741ae95f5bfa3b5729d90ed925d54a81d30bb
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2017-12-03 17:57:40 +0200

    master Remove server old architecture

--- src/lib/http-server-request-handler/constants/parser.js
+++
@@ -1,2 +0,0 @@
-export const REQUEST_START_LINE_REGEXP = new RegExp('(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)\\s([\\.a-zA-Z0-9\\w\\/-]+)\\s([\\.A-Z\\/\\d]+)');
-export const REQUEST_HEADER_REGEXP = new RegExp('([\\w-]+):\\s(.+)');
--- src/lib/http-server-request-handler/interface/RequestHandlerInterface.js
+++
@@ -1,5 +0,0 @@
-export interface RequestHandlerInterface {
-  config: Object,
-  processData(): void,
-  responseData(data: Object): void,
-}
