e2c21c33a9ef3255840b85dd035f5cc556c8e7ce
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-01-02 17:22:25 +0200

    prettier

--- tests/autotests/interacting.js
+++ tests/autotests/interacting.js
@@ -5,20 +5,20 @@ import EssentialHttpServer from '../../src/essential-http-server';
 
 const port = 3001;
 const server = new EssentialHttpServer({
-    rootDir: path.resolve(__dirname, '../../public/'),
-    port,
+  rootDir: path.resolve(__dirname, '../../public/'),
+  port,
 });
 const sitePath = `http://localhost:${port}`;
 
 server.start(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
 
-    await page.goto(sitePath);
-    await page.click('a');
-    await page.goto(`${sitePath}/no-page.html`);
+  await page.goto(sitePath);
+  await page.click('a');
+  await page.goto(`${sitePath}/no-page.html`);
 
-    browser.close();
+  browser.close();
 });
