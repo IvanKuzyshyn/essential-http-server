ed174a49eaaad95add1bffe5ec3a38fe18f68a1a
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-01-02 12:17:31 +0200

    master Implement async functions

--- src/essential-http-server/service/ResponseFormatter.js
+++ src/essential-http-server/service/ResponseFormatter.js
@@ -1,13 +1,16 @@
 import fs from 'fs';
 import path from 'path';
+import { promisify } from 'util';
 import { REQUEST_START_LINE_REGEXP, REQUEST_HEADER_REGEXP } from '../config/formatter';
 import setContentType from '../helper/contentType';
+
+const openAsync = promisify(fs.open);
+const readFileAsync = promisify(fs.readFile);
 
 export default class ResponseFormatter {
 
   constructor(rootDir, charset = 'utf-8') {
     this.config = {rootDir, charset};
-
 
     return {send: this.send};
   }
@@ -42,41 +45,31 @@ export default class ResponseFormatter {
     return request;
   };
 
-  getRequestedFileByPath = (filePath) => new Promise((resolve, reject) => {
-      fs.open(filePath, 'r', error => {
-        if(error) {
-          reject(error);
+  read = filePath => new Promise(async (resolve, reject) => {
+    try {
+      await openAsync(filePath, 'r');
+      const file = await readFileAsync(filePath);
 
-          return;
-        }
+      resolve(file);
+    } catch(error) {
+      reject(error);
+    }
+  });
 
-        fs.readFile(filePath, (error, data) => {
-          if(error) {
-            reject(error);
-
-            return;
-          }
-
-          resolve(data);
-        });
-      });
-    });
-
-  buildResponse = (request, socket) => {
+  buildResponse = async (request, socket) => {
     const requestFile = this.config.rootDir + request.uri;
 
-    this.getRequestedFileByPath(requestFile)
-      .then(data => {
-        const fileType = path.extname(requestFile).replace(/\./, '');
-        const response = this.prepareResponse(request, data, 200, fileType);
+    try {
+      const fileData = await this.read(requestFile);
+      const fileType = path.extname(requestFile).replace(/\./, '');
+      const response = this.prepareResponse(request, fileData, 200, fileType);
 
-        this.sendResponse(response, socket);
-      })
-      .catch(() => {
-        const response = this.prepareResponse(request, 'File not found', 404);
+      this.sendResponse(response, socket);
+    } catch(error) {
+      const response = this.prepareResponse(request, 'File not found', 404);
 
-        this.sendResponse(response, socket);
-      });
+      this.sendResponse(response, socket);
+    }
   };
 
   prepareResponse = (request, body, code, requestFileType = null) => {
